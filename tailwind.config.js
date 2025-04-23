02c3093c24f54228ce847ecaa24ec809ca0d15bb
damnpuppet <elmarionetist444@gmail.com>
2025-04-23 21:45:53 +0200

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        violet: {
+          400: '#a78bfa',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+          700: '#6d28d9',
+          900: '#4c1d95',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
